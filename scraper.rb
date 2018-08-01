4d9a93d22ca60f70fb53ed606dabeb00368ce9e1
Tony Bowden <tony@tmtm.com>
2018-08-01 18:47:28 +0100

    Always fetch existing matches

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,15 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/Congress_of_the_Federated_States_of_Micronesia',
   after: '//h2[contains(.,"Members")]',
   before: '//h2[contains(.,"See also")]',
   xpath: './/table//td[position() = last() - 2]//a[not(@class="new")]/@title',
-) 
+)
+existing = EveryPolitician::Index.new.country("Micronesia").lower_house.popolo.persons.map(&:wikidata).compact
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: names })
